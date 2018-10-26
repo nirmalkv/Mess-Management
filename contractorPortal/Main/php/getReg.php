dcf6f4332f2d9edadba4c1b324ae2570d1b2d020
Nirmal k v <nirmalkvaduva@gmail.com>
2018-10-26 13:54:21 +0530

    Updated and Formatted getreg.php

--- contractorPortal/Main/php/getReg.php
+++ contractorPortal/Main/php/getReg.php
@@ -15,27 +15,42 @@ $mess = $data['Name'];
 
 $sql = "SELECT * FROM mess_card WHERE Curr_mess = '$mess' AND Update_mess=1";
 $res = mysqli_query($MYSQL_CONNECTION,$sql);
+$month = date("m");
 
+$header = $mess.' Mess Registered Students - '.$month;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 $sheet->setTitle('Registered Students');
-$sheet->setCellValue('A1', 'Roll Number')
-	->setCellValue('B1', 'Mess Card Number');
-	// ->setCellValue('B1','Name')
-	// ->setCellValue('C1','Mobile Number')
-	// ->setCellValue('D1','Mess');
-$row = 2;
+$sheet->mergeCells('A1:E1');
+$sheet->setCellValue('A1', $header);
+$sheet->setCellValue('A2','SL No.')
+	->setCellValue('B2', 'Roll Number')
+	->setCellValue('C2', 'Mess Card Number')
+	->setCellValue('D2','Name')
+	->setCellValue('E2','Phone');
+$row = 3;
+$sl = 1;
 while ($data = mysqli_fetch_array($res)) {
-	$sheet->setCellValue('A'.$row , $data['Rollno'])
-		->setCellValue('B'.$row , $data['Curr_messcard']);
-		// ->setCellValue('C'.$row , $data['mobno'])
-		// ->setCellValue('D'.$row , $data['mess']);
+	$stud = "SELECT Name, Phone FROM student_info WHERE Rollno = {$data['Rollno']}";
+	$out = mysqli_query($MYSQL_CONNECTION,$stud);
+	$info = mysqli_fetch_array($out);
+	$sheet->setCellValue('A'.$row , $sl)
+		->setCellValue('B'.$row , $data['Rollno']);
+		->setCellValue('C'.$row , $data['curr_messcard'])
+		->setCellValue('D'.$row , $info['Name'])
+		->setCellValue('E'.$row, $info['Phone']);
 	$row++;
+	$sl++;
 }
 
+foreach (range('A','E') as $col) {
+	$sheet->getColumnDimension($col)->setAutoSize(true);  
+}
+
+$filename = $header .'xlsx';
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Encoding: none');
-header('Content-Disposition: attachment; filename=messreg.xlsx');
+header('Content-Disposition: attachment; filename=$filename');
 header('Cache-Control: max-age=0');
 
 $writer = new Xlsx($spreadsheet);
